7b0be4c88928151537d790607edfb7e588d5d474
tanui-ivy <ivy.tanui@student.moringaschool.com>
2025-04-13 18:08:06 +0300

    Task complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+  ('Mr. Chocolate', 20, 'M', 'Brown', 'Calm', 1),
+  ('Rowdy', 18, 'M', 'Black', 'Aggressive', 1),
+  ('Tabitha', 15, 'F', 'Cinnamon', 'Sweet', 1),
+  ('Sergeant Brown', 25, 'M', 'Brown', 'Strict', 0),
+  ('Melissa', 10, 'F', 'White', 'Playful', 1),
+  ('Grinch', 12, 'M', 'Green', 'Grumpy', 1),
+  ('Wendy', 8, 'F', 'Gray', 'Curious', 1),
+  (NULL, 5, 'M', 'Blue', 'Shy', 0);  -- unnamed bear
